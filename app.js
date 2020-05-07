62932b12f6e8390cfeca86957d20c7ce51e1df07
Iryna Predborska <iryna.predborska@gmail.com>
2020-05-07 17:21:51 +0200

    create a GET request for /articles route

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+mongoose.connect("mongodb://localhost: 27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", function (req, res) {
+    Article.find(function (err, results) {
+        if(!err) {
+            res.send(results);
+        } else {
+            res.send(err);
+        }
+    });
+});
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000.")
