1da3fbc02980978a860b19fe75e8418dbba069bd
Iryna Predborska <iryna.predborska@gmail.com>
2020-05-09 11:31:19 +0200

    add chained route handlers for a /articles route

--- app.js
+++ app.js
@@ -17,43 +17,81 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-//GET request to the /articles route
-app.get("/articles", function (req, res) {
-    Article.find(function (err, results) {
-        if(!err) {
-            res.send(results);
-        } else {
-            res.send(err);
-        }
+//Create a chained route handlers for a /articles route
+app.route("/articles")
+    .get(function (req, res) {
+        Article.find(function (err, results) {
+            if (!err) {
+                res.send(results);
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .post(function (req, res) {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+        newArticle.save(function (err) {
+            if (!err) {
+                res.send("Successfully added a new article!")
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .delete(function (req, res) {
+        Article.deleteMany(function (err) {
+            if (!err) {
+                res.send("Successfully deleted all articles!");
+            } else {
+                res.send(err);
+            }
+        });
     });
-});
 
-//POST request of creating a new article
-app.post("/articles", function (req, res) {
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
-    });
-    newArticle.save(function (err) {
-        if(!err) {
-            res.send("Successfully added a new article!")
-        } else {
-            res.send(err);
-        }
-    });
-});
 
 
-//DELETE all the articles
-app.delete("/articles", function (req, res) {
-    Article.deleteMany(function (err) {
-        if (!err) {
-            res.send("Successfully deleted all articles!");
-        } else {
-            res.send(err);
-        }
-    });
-});
+
+
+// //GET request to the /articles route
+// app.get("/articles", function (req, res) {
+//     Article.find(function (err, results) {
+//         if(!err) {
+//             res.send(results);
+//         } else {
+//             res.send(err);
+//         }
+//     });
+// });
+//
+// //POST request of creating a new article
+// app.post("/articles", function (req, res) {
+//     const newArticle = new Article({
+//         title: req.body.title,
+//         content: req.body.content
+//     });
+//     newArticle.save(function (err) {
+//         if(!err) {
+//             res.send("Successfully added a new article!")
+//         } else {
+//             res.send(err);
+//         }
+//     });
+// });
+//
+//
+// //DELETE all the articles
+// app.delete("/articles", function (req, res) {
+//     Article.deleteMany(function (err) {
+//         if (!err) {
+//             res.send("Successfully deleted all articles!");
+//         } else {
+//             res.send(err);
+//         }
+//     });
+// });
 
 
 
